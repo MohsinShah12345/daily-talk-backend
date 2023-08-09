8aefca2970292490680f581b2fb9a61f66317c56
MohsinShah12345 <mohsinking381@gmail.com>
2023-08-08 18:45:16 -0700

    Postgres Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PSQL_HOST,
+      port: parseInt(process.env.PSQL_PORT, 10),
+      username: process.env.PSQL_USERNAME,
+      password: process.env.PSQL_PASSWORD,
+      database: process.env.PSQL_DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  dotenv.config();
+  const app = await NestFactory.create(AppModule, { abortOnError: false });
+  // validation Pipes for data (dto) validation and tranformation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  await app.listen(process.env.PORT);
 }
 bootstrap();
